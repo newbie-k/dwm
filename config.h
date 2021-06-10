262cf04c7799953d6ec40669ac289495dcc8cc04
newbie-k <aomkhung0@hotmail.com>
2021-06-10 15:48:50 +0700

    selecfakefullscreen patch

--- config.h
+++ config.h
@@ -26,10 +26,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   isfakefullscreen monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           0,               -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           1,               -1 },
-	{ "Steam",    NULL,       NULL,       1 << 7,       0,           0,               -1 },
+ 	/* class      instance    title       tags mask     isfloating   isfakefullscreen monitor */
+ 	{ "Gimp",     NULL,       NULL,       0,            1,           0,               -1 },
+ 	{ "firefox",  NULL,       NULL,       1 << 8,       0,           1,               -1 },
+	{ "Steam",    NULL,       NULL,       1 << 7,       1,           0,               -1 },
 	{ "GNU Octave",NULL,      NULL,       0,            1,           0,               -1 },
 };
 
